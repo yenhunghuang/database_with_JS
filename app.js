59b7a6a0f2731e543e2f021afd4f4471bfdcb05c
yenhunghuang <53628808+yenhunghuang@users.noreply.github.com>
2024-07-23 22:31:30 +0800

    use_connection_pool_and_[?,?]_to_inject_sqlquerry_buy_url

--- app.js
+++ app.js
@@ -1,34 +1,37 @@
 import express from "express";
 import mysql from "mysql2";
 
-const connection = mysql.createConnection({
+const app = express();
+const port = 3000;
+
+const pool = mysql.createPool({
     host: "localhost",
     user: "root",
     password: "new_password",
     database: "TodoList",
 });
 
-connection.connect((err) => {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
-});
-
-const app = express();
+// 使用 promise 版本的连接池
+const promisePool = pool.promise();
 
 app.get("/", async (req, res) => {
-    connection.query(
-        "INSERT INTO Todo Value(1, 'My First Todo')",
-        (err, results, field) => {
-            res.send("Hello World!");
-            res.end();
-        }
-    );
+    const todoId = req.query.todo_id;
+    const todoTitle = req.query.todo_title;
+
+    try {
+        const [results] = await promisePool.query(
+            "INSERT INTO Todo (id, title) VALUES (?, ?)",
+            [todoId, todoTitle]
+        );
+        // 使用 results 中的数据
+        res.send(`Todo item added successfully! ID: ${results.insertId}`);
+    } catch (err) {
+        console.error("Error executing query", err);
+        res.status(500).send("An error occurred.");
+    }
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Example app listening on port 3000!");
 });
 
